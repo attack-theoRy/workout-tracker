2a078fec834b50f5036fbf901bd6795afc2098b5
Pirooz Wallace <PiroozWallace@outlook.com>
2021-02-16 21:00:10 -0800

    basic mongoose connection, front page working

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-const mongoose = require("mongoose")
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const Workout = require('../models/workout.js');
+
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -11,4 +29,33 @@ mongoose.connect(
       useFindAndModify: false
     }
   );
+  
+
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
+
+  // Get all workouts route and adding totalDuration
+app.get('/workouts', (req, res) => {
+    Workout.aggregate([{ $set: { totalDuration: { $sum: '$exercises.duration' }}}])
+      .sort({ day: 1 })
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+  
+  // Create workout route
+  app.post('/workouts', ({ body }, res) => {
+    Workout.create(body)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
   
