236f192626a1c9b6017e6169a39f0f0e61a4c67e
Pirooz Wallace <PiroozWallace@outlook.com>
2021-02-18 18:23:54 -0800

    fixing routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,66 +1,57 @@
 const router = require('express').Router();
 const Workout = require('../models/workout.js');
 
-// Get all workouts route and adding totalDuration
-  router.get('api/workouts', (req, res) => {
-    Workout.aggregate([{ $set: { totalDuration: { $sum: '$exercises.duration' }}}])
-      .sort({ day: 1 })
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.status(400).json(err);
-      });
-  });
-  
-  // Create workout route
-  router.post('api/workouts', ({ body }, res) => {
-    Workout.create(body)
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.status(400).json(err);
-      });
-  });
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-  // Add exercise to workout route
-router.put('/api/workouts/:id', ({ body, params }, res) => {
-    Workout.findOneAndUpdate({ _id: params.id }, { $push: { exercises: body } }, { new: true })
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.status(400).json(err);
-      });
-  });
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id, { $push: { exercises: body } }, { new: true, runValidators: true }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-  // Get range of last seven workouts route
-router.get('/api/workouts/range', (req, res) => {
-    Workout.aggregate([{ $set: { totalDuration: { $sum: '$exercises.duration' } } }])
-      .sort({ day: -1 })
-      .limit(7)
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.status(400).json(err);
-      });
-  });
-  
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({}).limit(7)
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-  // save exercises in bulk if app was offline
-router.post('/api/workouts-bulk', ( { body }, res) => {
-    Workout.findByIdAndUpdate(body[0].id, { $push: { exercises: { $each: body }}})
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.status(400).json(err);
-      });
-  });
-  
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
   module.exports = router;
   
